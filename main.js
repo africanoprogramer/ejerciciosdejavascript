7f0e6810d177d4bb90c7f82cd36ec3ec3878fa40
Braulio ONDO EDU MANGUE <elsbanedu@hotmail.com>
2022-12-28 17:54:41 +0100

    mi segundo

---
+++ main.js
@@ -0,0 +1,82 @@
+/**
+ * -------------------VARIABLES-------------------
+
+1.Create a variable called carName, assign the value Volvo to it. 1
+2.Create a variable called x, assign the value 50 to it. 1
+3. Muestra la suma de dos variables con getelmentby.... 1
+4.Create a variable called z, assign x + y to it, and display the result in an alert box. 1
+5.On one single line, declare three variables with the following names and values: 'ondo' 'edu' 30 1
+ */
+
+
+/**
+ 
+----------------------OPERADORES-------------------
+
+1.-Operdores de asignacion: asigna un valor al operando de la izquierda basado en el valor del operando de la derecha
+
+a) crea una variable asignale un numero, despues llama de nuevo esta variable y sumale +=5 y muestra el resultao 1
+b) lo mismo con la resta; 1
+c) lo mismo con la multiplicacion; 1
+d) lo mismo con la division; 1
+e) lo mismo con el resto -> ten encuenta que el resto lo conseguimos al numero mas cercano hacia abajo del numero divido, nos muestra el resto de una division. 1
+
+
+2.- Operdores aritmeticos: toman valores numericos ya sean literales o variables como sus operandos y retornan un valor numerico unico
+
+a) crea una variable y asignale un valor numerico y despues restale con este operador -- y luego muestralo en una varibale y lo muestras; 1
+b) haslo mismo con la suma 1
+c) divide dos numeros con variables diferentes. 1
+d) Divide 10 by 2, and alert the result: 1
+e) Alert the remainder(el resto de cuandp 15 es divido entre 9) when 15 is divided by 9. 1
+
+3.- Operadores de comparacion
+a) compara dos valores usando &&
+b) el mismo ejemplo pero despues retornando un valor
+
+
+ */
+
+/**
+ *----------------------TEMPLATE STRING-----------------------------
+
+1. Concate dos frases 1
+2. crea dos promt y concatena las frases, 1
+3. usa las comillas invertidas y ponle un div y anhadele una lista desordenada
+
+---------------------TYPOS DE DATOS-------------------------------
+
+- Escribir un programa que muestre un prompt donde el usuario ingresa un número, luego muestra otro prompt pidiendo otro número, finalmente el programa muestra  1
+- una alerta con el resultado de la suma. 1
+
+- Crear un programa donde el usuario ingrese en un prompt la temperatura en Celcius y el programa muestre en una alerta la temperatura en Fahrenheit. 1
+- Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, ejemplo: 5 / 10 igual 0.5 1
+- Si a = [1,2,3] crear un script que determine de muestre un alert del tipo de dato de a.
+
+
+
+ */
+
+
+
+
+
+let x = ['1','2','3'];
+
+
+
+ let resultado = typeof  x;
+   
+alert(resultado)
+
+
+
+
+
+
+let midiv = document.getElementById('miDiv');
+
+
+midiv.innerHTML = `<strong>El dato ${x} que has ingresado es un ${resultado}</strong>`
+
+
