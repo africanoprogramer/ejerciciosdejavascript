591a6fe1e0c256a127d96ed0c54c8a7d177a595a
Braulio ONDO EDU MANGUE <elsbanedu@hotmail.com>
2023-01-06 15:47:44 +0100

    ejercicios en la oficina

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
  * -------------------VARIABLES-------------------
 
-1.Create a variable called carName, assign the value Volvo to it. 2
-2.Create a variable called x, assign the value 50 to it. 2
-3. Muestra la suma de dos variables con getelmentby.... 2
-4.Create a variable called z, assign x + y to it, and display the result in an alert box. 2
-5.On one single line, declare three variables with the following names and values: 'ondo' 'edu' 30 2
+1.Create a variable called carName, assign the value Volvo to it. 3
+2.Create a variable called x, assign the value 50 to it. 3
+3. Muestra la suma de dos variables con getelmentby.... 3
+4.Create a variable called z, assign x + y to it, and display the result in an alert box. 3
+5.On one single line, declare three variables with the following names and values: 'ondo' 'edu' 30 3
  */
 
 /**
@@ -14,43 +14,52 @@
 
 1.-Operdores de asignacion: asigna un valor al operando de la izquierda basado en el valor del operando de la derecha
 
-a) crea una variable asignale un numero, despues llama de nuevo esta variable y sumale +=5 y muestra el resultao 2
-b) lo mismo con la resta; 2
-c) lo mismo con la multiplicacion; 2
-d) lo mismo con la division; 2
-e) lo mismo con el resto -> ten encuenta que el resto lo conseguimos al numero mas cercano hacia abajo del numero divido, nos muestra el resto de una division. 2
+a) crea una variable asignale un numero, despues llama de nuevo esta variable y sumale +=5 y muestra el resultao 3
+b) lo mismo con la resta; 3
+c) lo mismo con la multiplicacion; 3
+d) lo mismo con la division; 3
+e) lo mismo con el resto -> ten encuenta que el resto lo conseguimos al numero mas cercano hacia abajo del numero divido, nos muestra el resto de una division. 3
 
 
 2.- Operdores aritmeticos: toman valores numericos ya sean literales o variables como sus operandos y retornan un valor numerico unico
 
-a) crea una variable y asignale un valor numerico y despues restale con este operador -- y luego muestralo en una varibale y lo muestras; 2
-b) haslo mismo con la suma 2
-c) divide dos numeros con variables diferentes. 2
-d) Divide 10 by 2, and alert the result: 2
-e) Alert the remainder(el resto de cuandp 15 es divido entre 9) when 15 is divided by 9. 2
+a) crea una variable y asignale un valor numerico y despues restale con este operador -- y luego muestralo en una varibale y lo muestras; 3
+b) haslo mismo con la suma 3
+c) divide dos numeros con variables diferentes. 3
+d) Divide 10 by 2, and alert the result: 3
+e) Alert the remainder(el resto de cuandp 15 es divido entre 9) when 15 is divided by 9. 3
 
 3.- Operadores de comparacion 
-a) compara dos valores usando && 1
-b) el mismo ejemplo pero despues retornando un valor 2
+a) compara dos valores usando && 2
+b) el mismo ejemplo pero despues retornando un valor 3
 
 
+3.- Operaciones de comparacion 1
+son los que devuelven un resultado buleano. 1
+a) compara un texto si es igual al otro 1
+b) compara si un texto es  diferente a otro. 1
+c) compâra si un texto es diferente a otro comparando con el mismo tipo de dato 1
+d) compara si un numero es menor al otro 1
+e) compara si la dimencion de un texto es mayor al otro 1
+f) compara dos valores un verdadero y otro falso con ||, ten encuenta que este operador coge el valor verdadero a diferencia de && que returna falso si tana solo un valor es falso. 1
+g) Con el operador correcto devuelve el valor direfente al que imprimes (true or false) 1
  */
 
 /**
  *----------------------TEMPLATE STRING-----------------------------
 
-1. Concate dos frases 2
-2. crea dos promt y concatena las frases, 2
-3. usa las comillas invertidas y ponle un div y anhadele una lista desordenada 1
+1. Concate dos frases 3
+2. crea dos promt y concatena las frases, 3
+3. usa las comillas invertidas y ponle un div y anhadele una lista desordenada 2
 
 ---------------------TYPOS DE DATOS-------------------------------
 
-- Escribir un programa que muestre un prompt donde el usuario ingresa un número, luego muestra otro prompt pidiendo otro número, finalmente el programa muestra  2
-- una alerta con el resultado de la suma. 2
+- Escribir un programa que muestre un prompt donde el usuario ingresa un número, luego muestra otro prompt pidiendo otro número, finalmente el programa muestra  3
+- una alerta con el resultado de la suma. 3
 
-- Crear un programa donde el usuario ingrese en un prompt la temperatura en Celcius y el programa muestre en una alerta la temperatura en Fahrenheit. 1
-- Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, ejemplo: 5 / 10 igual 0.5 2
-- Si a = [1,2,3] crear un script que determine de muestre un alert del tipo de dato de a. 2
+- Crear un programa donde el usuario ingrese en un prompt la temperatura en Celcius y el programa muestre en una alerta la temperatura en Fahrenheit. 2
+- Pedir un número a un usuario a través de un prompt y luego dividirlo por 10, ejemplo: 5 / 10 igual 0.5 3
+- Si a = [1,2,3] crear un script que determine de muestre un alert del tipo de dato de a. 3
 
  */
 
@@ -59,39 +68,73 @@ b) el mismo ejemplo pero despues retornando un valor 2
  * 1.- Hacer un ejercicio con if else si tiene menos de 18 anhos no puede votar y si tiene 18  puede votar y si tiene mas de 18 puede votar y conducir.
  * 2.- Has un ejemplo que al seleccionar el tipo de clima te muestra unas ecrituras, tengo hacer un select y coger una p desde el html 
  * 3.- Hacer un ejercicio con switch
- * 
+ * 4.- Hacer un ejercicio de tres jovenes que compran helados dependiendo la cantidad de dinero que tienen, el program les indica el . utilizando >= 
  */
 
 /**
  * ---------------------------------------ARRAYZ----------------------------------------
  * 1.-crea un array de tres frutas, anhade una fruta y se posiciona en la ultima posicion, otra fruta que se posicione en la primera posicion, cambia el valor del tercer array, elimina el primer array, elimina el ultimo array.
  * 2.- En eeste desafío vas a recibir un parámetro arraySecreto en la función solution. Debes retornar true si el primer elemento del arraySecreto es de tipo string y debes retornar false si es de cualquier otro tipo.
+ * 3.-
  */
 
 /**
  * --------------------------------------LOOPS------------------------------------------
  * 1.-Cuenta los numeros hasta 10 for
  * 2.- has funcion que salude a los estudiantes.
- * 
+ * 3.- lo mismo pero con for of
+ * 4.- ejercuta un numero con while
+ * 5.- saluda a estudiantes con  while
+ * 6.- hacer lo mismo con do while clase numero 20 platzi
+
  
  */
 
-let estudiantes = ['Francisco','Braulio','Bodien']
+/**
+ * --------------------------------------OBJETOS------------------------------------------
+ * 1.-Hacer un objeto de un auto;
+ * 2.- crear un input select en html y crear un objeto de un veiculo que al seleccionar algo el select sea la informacion que te retore el objeto.
+ * 3.- crea un funcion dentro del objeto que retorne la informacion que se encuentra dentro del objeto.
+ * 4.- crear funcion constructora de un auto despues llama una instancia con un  nuevo auto
+ * 5.- crea una tarrea que agrega 7 autos y muestralos
+ * 6.- hacer una funcion que solo reciba un objeto como parametro, despues cuando le introduzcamos lo datos del auto si el auto no tiene licencia de conducir que lo traiga false
 
-estudiantes.push('Benjamin')
+ 
+ */
 
-function Saludar(estudiantes) {
-    return `Hola ${estudiantes}`
+
+/*
+
+
+
+ */
+ 
+
+let element =  document.querySelector('p');
+
+let t = [1,2,3];
+
+function solucion(a) {
+
+    return typeof a
     
 }
 
 
 
-for (let i = 0; i < estudiantes.length; i++) {
+console.log(solucion(t))
 
 
-    console.log(Saludar(estudiantes[i]))
+
+
+
+
+element.innerHTML = `<ul><li>${t[0]}</li><li>${t[1]}</li><li>${t[2]}</li></ul>`
+
+
+
+
+
+
+
     
-    
-}
-
